7ea5736517a42e8b55d57003b52b6a16140e83f1
Павел Чеклин <p.cheklin@corp.mail.ru>
2022-06-23 14:34:43 +0300

    udpated sql ad schemes

--- cmd/cobra-client/main.go
+++ cmd/cobra-client/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-	"jirno/internal/app/cobra-cli"
+	cobra_cli "jirno/internal/app/cobra-cli"
+	"math/rand"
 )
 
 func main() {
 	err := cobra_cli.RunApp()
+	var i int
+	i = rand.Intn(10)
+	if i != 1 {
+		i++
+	}
 	if err != nil {
 		fmt.Println("Application ended with: ", err)
 	}
